f2fd8d2240b108868ec2e8e4ee4d489482a75450
Scott Elder <scottelder.tx@gmail.com>
2021-12-08 07:56:57 -0500

    Added shell of classes for day 8

---
+++ AdventOfCode2021/Day8_SignalDisplay.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day8_SignalDisplay
+    {
+        public static int CountDigits_2_4_5_7(string[] signalsAndOutputs)
+        {
+            return 0;
+        }
+    }
+}
---
+++ AdventOfCode2021/Tests/SignalDisplayTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestFixture]
+    internal class SignalDisplayTests
+    {
+        // Day 8
+        [Test]
+        public void TestCountingEasyDigits()
+        {
+            // arrange
+            var input = File.ReadLines("Tests/SignalDisplayInputV1.txt").ToArray();
+
+            // act
+            var result = Day8_SignalDisplay.CountDigits_2_4_5_7(input);
+
+            // assert
+            Assert.AreEqual(26, result);
+        }
+    }
+}
