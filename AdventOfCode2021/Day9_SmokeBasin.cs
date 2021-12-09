25daf3ab597eba4da60d5cac7dbadcc295a478e5
Scott Elder <scottelder.tx@gmail.com>
2021-12-09 09:43:06 -0500

    Completed day 9, part I

---
+++ AdventOfCode2021/Day9_SmokeBasin.cs
@@ -0,0 +1,37 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day9_SmokeBasin
+    {
+        public static int FindLowPoints(int[][] grid)
+        {
+            int sum = 0;
+            int maxRowIndex = grid.Length - 1;
+            for (int i = 0; i < grid.Length; i++)
+            {
+                int maxColumnIndex = grid[i].Length - 1;
+
+                // set up, down, left, right indexes 
+                int up = i - 1 >= 0 ? i - 1 : i + 1;
+                int down = i + 1 <= maxRowIndex ? i + 1 : i - 1;
+
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    int left = j - 1 >= 0 ? j - 1 : j + 1;
+                    int right = j + 1 <= maxColumnIndex ? j + 1 : j - 1;
+
+                    if (grid[i][j] < grid[up][j] &&
+                        grid[i][j] < grid[down][j] &&
+                        grid[i][j] < grid[i][left] &&
+                        grid[i][j] < grid[i][right])
+                    {
+                        sum += grid[i][j] + 1;
+                    }
+                }
+
+                
+            }
+               
+            return sum;
+        }       
+    }
+}
---
+++ AdventOfCode2021/Tests/SmokeBasinTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestFixture]
+    internal class SmokeBasinTests
+    {
+        // Day 9
+        [Test]
+        public void TestFindingLowPoints()
+        {
+            // arrange - read grid from file and load into jagged array 
+            int[][] input = File.ReadAllLines("Tests/SmokeBasinGridV1.txt")
+                   .Select(l => l.ToCharArray().Select(i => (int)Char.GetNumericValue(i)).ToArray())
+                   .ToArray();
+
+            // act
+            var result = Day9_SmokeBasin.FindLowPoints(input);
+
+            // assert
+            Assert.AreEqual(512, result);
+        }
+    }
+}
