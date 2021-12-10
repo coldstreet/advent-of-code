327df1db5a9f075bf6002fc38c73713e3a88a973
Scott Elder <scottelder.tx@gmail.com>
2021-12-10 08:14:31 -0500

    Add shell classes for Day 10

---
+++ AdventOfCode2021/Tests/SyntaxScoringTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestFixture]
+    internal class SyntaxScoringTests
+    {
+        // Day 9
+        [Test]
+        public void TestFindingLowPoints()
+        {
+            // arrange - read grid from file and load into jagged array 
+            var input = File.ReadLines("Tests/SyntaxChunksV1.txt").ToArray();
+
+            // act
+            var result = Day10_SyntaxScoring.CalculateScore(input);
+
+            // assert
+            Assert.AreEqual(26397, result);
+        }
+    }
+}
