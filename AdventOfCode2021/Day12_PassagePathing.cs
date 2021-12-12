b70c1d88b42930f608922e8c7274064b1eaac480
Scott Elder <scottelder.tx@gmail.com>
2021-12-12 10:45:10 -0500

    Created initial classes for day 12

---
+++ AdventOfCode2021/Day12_PassagePathing.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day12_PassagePathing
+    {
+        public static int CountAllPaths(string[] input)
+        {
+            int totalPaths = 0;
+
+
+            return totalPaths;
+        }
+
+    }
+}
+
---
+++ AdventOfCode2021/Tests/PassagePathingTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestFixture]
+    internal class PassagePathingTests
+    {
+        // Day 12
+        [Test]
+        public void TestCountingEasyDigits()
+        {
+            // arrange
+            var input = File.ReadLines("Tests/PassagePathSegmentsV1.txt").ToArray();
+
+            // act
+            var result = Day12_PassagePathing.CountAllPaths(input);
+
+            // assert
+            Assert.AreEqual(10, result);
+        }
+    }
+}
