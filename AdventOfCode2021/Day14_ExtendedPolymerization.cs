802506e84b3108a400fa3c46dfcdca52d17fd083
Scott Elder <scottelder.tx@gmail.com>
2021-12-14 12:27:49 -0500

    Added shell classes for Day 14

---
+++ AdventOfCode2021/Day14_ExtendedPolymerization.cs
@@ -0,0 +1,42 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day14_ExtendedPolymerization
+    {
+        public static int FindProductOfLeastAndMostLetters(string[] input)
+        {
+            var code = input[0];
+
+            var insertInstructions = input
+                .Take(input.Length - 2)
+                .Select(l => l.Split(" -> ").ToArray());
+
+            // build pairs list
+            var pairs = new HashSet<string>();
+            for (int i = 0; i < code.Length - 1; i++)
+            {
+                var pair = code.Substring(i, 2);
+                if (!pairs.Contains(pair))
+                {
+                    pairs.Add(pair);
+                }
+            }
+
+            // build alphabit counts
+            var alphabitCounts = new Dictionary<char, int>(26);
+            foreach(var c in code)
+            {
+                if (!alphabitCounts.ContainsKey(c))
+                {
+                    alphabitCounts.Add(c, 1);
+                    continue;
+                }
+
+                alphabitCounts[c]++;
+            }
+
+
+            return 0;
+        }
+    }
+}
+
---
+++ AdventOfCode2021/Tests/ExtendedPolymerizationTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestFixture]
+    internal class ExtendedPolymerizationTests
+    {
+        // Day 13
+        [Test]
+        public void TestFindProductOfLeastAndMostLetters()
+        {
+            // arrange - read grid from file and load into jagged array 
+            var input = File.ReadLines("Tests/ExtendedPolymerizationInputV1.txt").ToArray();
+
+            // act
+            var result = Day14_ExtendedPolymerization.FindProductOfLeastAndMostLetters(input);
+
+            // assert
+            Assert.AreEqual(1588, result);
+        }
+
+    }
+}
