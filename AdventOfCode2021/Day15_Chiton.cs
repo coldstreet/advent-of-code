85d17d4b974912f4578414448b673812ae99d7fc
Scott Elder <scottelder.tx@gmail.com>
2021-12-17 10:12:31 -0500

    Find problem (important with Dijkstra's algo to process next node that is least weighted)

--- AdventOfCode2021/Day15_Chiton.cs
+++ AdventOfCode2021/Day15_Chiton.cs
@@ -3,7 +3,6 @@
     public class Day15_Chiton
     {
         private int[,] _caveRiskGrid = new int[0,0];
-
         private HashSet<(int, int)> _visited = new HashSet<(int, int)>();
         private int[,] _weightedRiskGrid = new int[0, 0];
 
@@ -32,6 +31,70 @@
             FindLeastEnergyPath();
 
             return _weightedRiskGrid[lengthX - 1, lengthY - 1];
+        }
+
+        private void FindLeastEnergyPath()
+        {
+            var lengthX = _caveRiskGrid.GetLength(1);
+            var lengthY = _caveRiskGrid.GetLength(0);
+
+            var priorityQueue = new PriorityQueue<(int, int), int>();
+            priorityQueue.Enqueue((0, 0), 0);
+
+            while (true)
+            {
+                (int x, int y) = priorityQueue.Dequeue();
+                if (x == lengthX - 1 && y == lengthY - 1)
+                {
+                    break;
+                }
+
+                if (IsValidSquareToVisit(x + 1, y))
+                {
+                    UpdateWeightedRiskGrid(x + 1, y, _weightedRiskGrid[x, y], priorityQueue);
+                }
+
+                // down
+                if (IsValidSquareToVisit(x, y + 1))
+                {
+                    UpdateWeightedRiskGrid(x, y + 1, _weightedRiskGrid[x, y], priorityQueue);
+                }
+
+                // up
+                if (IsValidSquareToVisit(x, y - 1))
+                {
+                    UpdateWeightedRiskGrid(x, y - 1, _weightedRiskGrid[x, y], priorityQueue);
+                }
+
+                // left
+                if (IsValidSquareToVisit(x - 1, y))
+                {
+                    UpdateWeightedRiskGrid(x - 1, y, _weightedRiskGrid[x, y], priorityQueue);
+                }
+
+                _visited.Add((x, y));
+            }
+        }
+
+        private void UpdateWeightedRiskGrid(int x, int y, int weightedRiskLevel, PriorityQueue<(int, int), int> priorityQueue)
+        {
+            if (weightedRiskLevel + _caveRiskGrid[x, y] < _weightedRiskGrid[x, y])
+            {
+                _weightedRiskGrid[x, y] = weightedRiskLevel + _caveRiskGrid[x, y];
+                priorityQueue.Enqueue((x, y), _weightedRiskGrid[x, y]);
+            }
+        }
+
+        private bool IsValidSquareToVisit(int x, int y)
+        {
+            if (_visited.Contains((x, y)))
+            {
+                return false;
+            }
+
+            var lengthX = _caveRiskGrid.GetLength(1);
+            var lengthY = _caveRiskGrid.GetLength(0);
+            return y >= 0 && y < lengthY && x >= 0 && x < lengthX;
         }
 
         private void EnlargeInput(int[,] caveRiskGrid)
@@ -77,118 +140,6 @@
                 }
             }
         }
-
-        private void FindLeastEnergyPath()
-        {
-            var lengthX = _caveRiskGrid.GetLength(1);
-            var lengthY = _caveRiskGrid.GetLength(0);
-            for(int y = 0; y < lengthY; y++)
-            {
-                for(int x = 0; x < lengthX; x++)
-                {
-                    // right
-                    if (IsValidSquareToVisit(x + 1, y))
-                    {
-                        UpdateWeightedRiskGrid(x + 1, y, _weightedRiskGrid[x, y]);
-                    }
-
-                    // down
-                    if (IsValidSquareToVisit(x, y + 1))
-                    {
-                        UpdateWeightedRiskGrid(x, y + 1, _weightedRiskGrid[x, y]);
-                    }
-
-                    // up
-                    if (IsValidSquareToVisit(x, y - 1))
-                    {
-                        UpdateWeightedRiskGrid(x, y - 1, _weightedRiskGrid[x, y]);
-                    }
-
-                    // left
-                    if (IsValidSquareToVisit(x - 1, y))
-                    {
-                        UpdateWeightedRiskGrid(x - 1, y, _weightedRiskGrid[x, y]);
-                    }
-
-                    _visited.Add((x, y));
-                }
-            }
-        }
-
-        private void UpdateWeightedRiskGrid(int x, int y, int weightedRiskLevel)
-        {
-            if (weightedRiskLevel + _caveRiskGrid[x, y] < _weightedRiskGrid[x, y])
-            {
-                _weightedRiskGrid[x, y] = weightedRiskLevel + _caveRiskGrid[x, y];
-            }
-        }
-
-        private bool IsValidSquareToVisit(int x, int y)
-        {
-            if (_visited.Contains((x, y)))
-            {
-                return false;
-            }
-
-            var lengthX = _caveRiskGrid.GetLength(1);
-            var lengthY = _caveRiskGrid.GetLength(0);
-            return y >= 0 && y < lengthY && x >= 0 && x < lengthX;
-        }
-
-        // old inefficient, first solution
-        //private void VisitSquare(int x, int y, int currentRiskAccumulation, HashSet<(int, int)> visited)
-        //{
-        //    if (visited.Contains((x, y)))
-        //    {
-        //        return;
-        //    }
-
-        //    visited.Add((x, y));
-
-        //    var updatedRiskLevel = currentRiskAccumulation + _caveRiskGrid[x, y];
-        //    if (updatedRiskLevel >= _minPathSum)
-        //    {
-        //        // abandon path
-        //        return;
-        //    }
-
-        //    if (x == _caveRiskGrid.GetLength(1) - 1 && y == _caveRiskGrid.GetLength(0) - 1)
-        //    {
-        //        // reached end square
-        //        if (updatedRiskLevel < _minPathSum)
-        //        {
-        //            _minPathSum = updatedRiskLevel;
-        //        }
-
-        //        return;
-        //    }
-
-        //    // right
-        //    if (IsValidSquareToVisit(x + 1, y))
-        //    {
-        //        VisitSquare(x + 1, y, updatedRiskLevel, new HashSet<(int, int)>(visited));
-        //    }
-
-        //    // down
-        //    if (IsValidSquareToVisit(x, y + 1))
-        //    {
-        //        VisitSquare(x, y + 1, updatedRiskLevel, new HashSet<(int, int)>(visited));
-        //    }
-
-        //    // up
-        //    if (IsValidSquareToVisit(x, y - 1))
-        //    {
-        //        VisitSquare(x, y - 1, updatedRiskLevel, new HashSet<(int, int)>(visited));
-        //    }
-
-        //    // left
-        //    if (IsValidSquareToVisit(x - 1, y))
-        //    {
-        //        VisitSquare(x - 1, y, updatedRiskLevel, new HashSet<(int, int)>(visited));
-        //    }
-
-        //    return;
-        //}
     }
 }
 
