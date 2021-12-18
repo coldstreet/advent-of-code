e5811d3322f5f0bc28af33c600ff64e0145eac0d
Scott Elder <scottelder.tx@gmail.com>
2021-12-17 22:12:43 -0500

    Added shell classes for Day 16

---
+++ AdventOfCode2021/Day16_PacketDecoder.cs
@@ -0,0 +1,13 @@
+﻿namespace AdventOfCode2021
+{
+    public static class Day16_PacketDecoder
+    {
+        public static long DecodeAndSumVersionNumbers(string[] input)
+        {
+            
+
+            return 0;
+        }
+    }
+}
+
---
+++ AdventOfCode2021/Tests/PacketDecoderTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2021.Tests
+{
+    [TestFixture]
+    internal class PacketDecoderTests
+    {
+        // Day 16
+        [Test]
+        public void TestDecodingPacket()
+        {
+            // arrange - read grid from file and load into jagged array 
+            var input = File.ReadLines("Tests/PacketDecoderInputV1.txt").ToArray();
+
+            // act
+            var result = Day16_PacketDecoder.DecodeAndSumVersionNumbers(input);
+
+            // assert
+            Assert.AreEqual(16, result);
+        }
+    }
+}
