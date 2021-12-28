f2df2adec41b1623e365a9c022579149bf6ce41a
Scott Elder <scottelder.tx@gmail.com>
2021-12-27 19:37:36 -0500

    Explode is now working

--- AdventOfCode2021/Day18_Snailfish.cs
+++ AdventOfCode2021/Day18_Snailfish.cs
@@ -5,111 +5,142 @@ namespace AdventOfCode2021
 {
     public static class Day18_Snailfish
     {
+        private class PriorityPair
+        {
+            public int Left { get; set; }
+            public int Right { get; set; }
+            public int Priority { get; set; }
+
+            public PriorityPair(int left, int right, int priority)
+            {
+                Left = left;
+                Right = right;
+                Priority = priority;
+            }
+        }
+
         public static long SumAllNumbers(string[] input)
         {
 
             // e.g., [[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]    
             // [[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]
-            var previousPriorityQueues = new PriorityQueue<(int, int), int>[0];
+            var previousPriorityPairs = new List<PriorityPair>();
             foreach (var line in input)
             {
                 var pairCount = line.Where(x => x == '[').Count();
 
-                // build priority queues for line
-                var priorityQueues = BuildQueues(line, pairCount);
-                if (previousPriorityQueues.Count() > 0)
+                // build priority pairs list based on line
+                var priorityPairs = BuildPriorityPairs(line, pairCount);
+                if (previousPriorityPairs.Count() > 0)
                 {
-                    Merge(previousPriorityQueues, priorityQueues);
+                    Merge(previousPriorityPairs, priorityPairs);
                 }
 
                 // build new line after explode and split operations and adding to next line
-                Explode();
+                Explode(priorityPairs, pairCount);
 
-                StringSplitOptions();
+                Split();
 
-                previousPriorityQueues = priorityQueues;
+                previousPriorityPairs = priorityPairs;
             }
 
-            var result = GetMagnitude(previousPriorityQueues);
+            var result = GetMagnitude(previousPriorityPairs);
             
 
             return result;
         }
 
-        private static void Explode()
+        private static void Explode(List<PriorityPair> priorityPairs, int pairsCount)
         {
+            var minPriority = priorityPairs.OrderBy(p => p.Priority).First().Priority;
+
+            if (pairsCount - minPriority >= 4)
+            {
+                for (int i = 0; i < priorityPairs.Count(); i++)
+                {
+                    // find the deepest pair to "explode"
+                    if (priorityPairs[i].Priority == minPriority)
+                    {
+                        // explode
+                        var left = priorityPairs[i].Left;
+                        var right = priorityPairs[i].Right;
+
+                        var nextLeftPriority = i - 1 >= 0 ? priorityPairs[i - 1].Priority : int.MaxValue;
+                        var nextRightPriority = i + 1 < priorityPairs.Count() ? priorityPairs[i + 1].Priority : int.MaxValue;
+
+                        // add left amount to valid pair on left  
+                        // look "left" (i.e., up in list)
+                        for (int j = i - 1; j >= 0; j--)
+                        {
+                            if (priorityPairs[j].Left != -1)
+                            {
+                                if (priorityPairs[j].Right == -1 && nextLeftPriority < nextRightPriority)
+                                {
+                                    priorityPairs[j].Right = 0;
+                                }
+                                
+                                priorityPairs[j].Left += left;
+                                break;
+                            }
+                        }
+
+                        // add right amount to valid pair on right  
+                        // look "right" (i.e., down in list)
+                        for (int j = i + 1; j < priorityPairs.Count(); j++)
+                        {
+                            if (priorityPairs[j].Right != -1)
+                            {
+                                if (priorityPairs[j].Left == -1 && nextRightPriority < nextLeftPriority)
+                                {
+                                    priorityPairs[j].Left = 0;
+                                }
+                                
+                                priorityPairs[j].Right += right;
+                                break;
+                            }
+                        }
+
+                        // adjust priority numbers for each pair (+1 per pair)
+                        for (int j = 0; j < priorityPairs.Count(); j++)
+                        {
+                            priorityPairs[j].Priority--;
+                        }
+
+                        // remove the "exploded" pair
+                        priorityPairs.RemoveAt(i);
+
+                        // check for more deep nested pairs to explode
+                        Explode(priorityPairs, pairsCount - 1);
+
+                        break;
+                    }
+                }
+            }
+
             return;
         }
 
-        private static int GetMagnitude(PriorityQueue<(int, int), int>[] previousPriorityQueues)
+        private static void Split()
+        {
+            return; // todo
+        }
+
+        private static int GetMagnitude(List<PriorityPair> priorityPairs)
         {
             return 0; // todo
         }
 
-        private static void StringSplitOptions()
+        private static void Merge(List<PriorityPair> previousPriorityPairs, List<PriorityPair> priorityPairs)
         {
             return; // todo
         }
 
-        private static void Merge(PriorityQueue<(int, int), int>[] previousPriorityQueues, PriorityQueue<(int, int), int>[] priorityQueues)
+        private static List<PriorityPair> BuildPriorityPairs(string line, int pairCount)
         {
-            return; // todo
-        }
-
-        private static StringBuilder RebuildLine(PriorityQueue<(int, int), int>[] priorityQueues)
-        {
-            StringBuilder newLine = new StringBuilder();
-            for (int i = 0; i < priorityQueues.Length; i++)
-            {
-                var queue = priorityQueues[i];
-                var pairsInQueue = queue.Count;
-                StringBuilder subline = new StringBuilder();
-                while (pairsInQueue > 0)
-                {
-                    (var left, var right) = queue.Dequeue();
-                    if (left != -1 && right != -1)
-                    {
-                        subline.Append($"[{left},{right}]");
-                    }
-                    else if (right == -1)
-                    {
-                        subline.Insert(0, $"[{left},");
-                        subline.Append(']');
-                    }
-                    else
-                    {
-                        subline.Insert(0, '[');
-                        subline.Append($",{right}]");
-                    }
-                    pairsInQueue--;
-                }
-
-                newLine.Append(subline);
-                if (i - 1 >= 0 && queue.Count == priorityQueues[i - 1].Count)
-                {
-                    newLine.Insert(0, '[');
-                    newLine.Append(']');
-                }
-
-                if (i + 1 < priorityQueues.Length)
-                {
-                    newLine.Append(",");
-                }
-
-            }
-
-            return newLine;
-        }
-
-        private static PriorityQueue<(int, int), int>[] BuildQueues(string line, int pairCount)
-        {
-            var queueCount = Regex.Matches(line, "\\],\\[").Count() == 0 ? 1 : Regex.Matches(line, "\\],\\[").Count() + 1;
             var inputChars = line.Replace(",", "").ToCharArray();
 
-            var priorityQueues = new PriorityQueue<(int, int), int>[queueCount];
+            var priorityPairs = new List<PriorityPair>();
             var nestingDepth = pairCount + 1;
-            int queueIndex = 0;
-            priorityQueues[queueIndex] = new PriorityQueue<(int, int), int>();
             for (int i = 0; i < inputChars.Length; i++)
             {
                 char previousChar = i - 1 >= 0 ? inputChars[i - 1] : '\0';
@@ -118,11 +149,10 @@ namespace AdventOfCode2021
                 if (character == '[')
                 {
                     nestingDepth--;
-                    if (previousChar == ']')
-                    {
-                        queueIndex++;
-                        priorityQueues[queueIndex] = new PriorityQueue<(int, int), int>();
-                    }
+                    //if (previousChar == ']')
+                    //{
+                    //    nestingDepth++;
+                    //}
                     continue;
                 }
 
@@ -134,20 +164,20 @@ namespace AdventOfCode2021
 
                 if (char.IsNumber(nextChar))
                 {
-                    priorityQueues[queueIndex].Enqueue(((int)char.GetNumericValue(character), (int)char.GetNumericValue(nextChar)), nestingDepth);
+                    priorityPairs.Add(new PriorityPair((int)char.GetNumericValue(character), (int)char.GetNumericValue(nextChar), nestingDepth));
                     i++;
                 }
                 else if (nextChar == ']')
                 {
-                    priorityQueues[queueIndex].Enqueue((-1, (int)char.GetNumericValue(character)), nestingDepth);
+                    priorityPairs.Add(new PriorityPair(-1, (int)char.GetNumericValue(character), nestingDepth));
                 }
                 else
                 {
-                    priorityQueues[queueIndex].Enqueue(((int)char.GetNumericValue(character), -1), nestingDepth);
+                    priorityPairs.Add(new PriorityPair((int)char.GetNumericValue(character), -1, nestingDepth));
                 }
             }
 
-            return priorityQueues;
+            return priorityPairs;
         }
     }
 }
