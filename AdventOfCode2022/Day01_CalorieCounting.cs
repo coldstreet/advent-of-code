be415e79b4cbc5edd99a69a71f0179453333a854
Scott Elder <scott.elder@relativity.com>
2022-12-01 20:25:13 -0500

    Day 1 for 2022 done

---
+++ AdventOfCode2022/Day01_CalorieCounting.cs
@@ -0,0 +1,39 @@
+﻿namespace AdventOfCode2022
+{
+    public static class Day01_CalorieCounting
+    {
+        public static long FindMaxCalorieForAnElf(string[] input)
+        {
+            var caloriesPerElf = ParseInputForTotalCaloriesPerElf(input);
+
+            return caloriesPerElf.Max();
+        }
+
+        public static long FindMaxCalorieForTopThreeElves(string[] input)
+        {
+            var caloriesPerElf = ParseInputForTotalCaloriesPerElf(input);
+
+            return caloriesPerElf.OrderByDescending(x => x).Take(3).Sum();
+        }
+
+        private static List<int> ParseInputForTotalCaloriesPerElf(string[] input)
+        {
+            var caloriesPerElf = new List<int>();
+            var sum = 0;
+            foreach (var item in input)
+            {
+                if (item.Length == 0)
+                {
+                    caloriesPerElf.Add(sum);
+                    sum = 0;
+                    continue;
+                }
+
+                sum += int.Parse(item);
+            }
+
+            caloriesPerElf.Add(sum);
+            return caloriesPerElf;
+        }
+    }
+}
---
+++ AdventOfCode2022/Tests/Day01_CalorieCountingTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestFixture]
+    internal class Day01_CalorieCountingTests
+    {
+        [Test]
+        public void TestFindMaxCalorieForAnElf()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day01_CalorieCountingTests.txt").ToArray();
+
+            // act
+            var result = Day01_CalorieCounting.FindMaxCalorieForAnElf(input);
+
+            // assert
+            Assert.AreEqual(68923, result);
+        }
+
+        [Test]
+        public void FindMaxCalorieForTopThreeElves()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day01_CalorieCountingTests.txt").ToArray();
+
+            // act
+            var result = Day01_CalorieCounting.FindMaxCalorieForTopThreeElves(input);
+
+            // assert
+            Assert.AreEqual(200044, result);
+        }
+    }
+}
