9ecc197e542af656842f4cb18a2616a733f03be8
Scott Elder <scott.elder@relativity.com>
2022-12-03 09:13:57 -0500

    Added template files

---
+++ AdventOfCode2022/Day99_ChallengeName.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2022
+{
+    public static class Day99_ChallengeName
+    {
+        public static long SomeAction(string[] input)
+        {
+            return 0;
+        }
+    }
+}
+
---
+++ AdventOfCode2022/Tests/Day99_ChallengeNameTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestFixture]
+    internal class Day99_ChallengeNameTests
+    {
+        [Test]
+        public void TestSomeAction()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/ChallengeNameInputV1.txt").ToArray();
+
+            // act
+            var result = Day99_ChallengeName.SomeAction(input);
+
+            // assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
