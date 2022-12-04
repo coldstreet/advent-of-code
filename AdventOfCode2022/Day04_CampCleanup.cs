b2c218a8749f3b987427c6415b9bea850e6935e7
Scott Elder <scott.elder@relativity.com>
2022-12-04 12:34:09 -0500

    Day 4 done

---
+++ AdventOfCode2022/Day04_CampCleanup.cs
@@ -0,0 +1,76 @@
+﻿namespace AdventOfCode2022
+{
+    public static class Day04_CampCleanup
+    {
+        public static long FindFullyOverlappingPairs(string[] input)
+        {
+            var overlappingPairs = 0;
+
+            foreach (var pair in input)
+            {
+                var x = pair
+                    .Split('-', ',')
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+                
+                if (DoPairsFullyOverlap(x[0], x[1], x[2], x[3]))
+                {
+                    overlappingPairs++;
+                }
+            }
+
+            return overlappingPairs;
+        }
+
+        public static long FindOverlappingPairs(string[] input)
+        {
+            var overlappingPairs = 0;
+
+            foreach (var pair in input)
+            {
+                var x = pair
+                    .Split('-', ',')
+                    .Select(x => int.Parse(x))
+                    .ToArray();
+
+                if (DoPairsOverlap(x[0], x[1], x[2], x[3]))
+                {
+                    overlappingPairs++;
+                }
+            }
+
+            return overlappingPairs;
+        }
+
+        private static bool DoPairsFullyOverlap(int min1, int max1, int min2, int max2)
+        {
+            if (min1 >= min2 && max1 <= max2)
+            {
+                return true;
+            }
+
+            return min2 >= min1 && max2 <= max1;
+        }
+
+        private static bool DoPairsOverlap(int min1, int max1, int min2, int max2)
+        {
+            if (min1 >= min2 && max1 <= max2)
+            {
+                return true;
+            }
+
+            if (min2 >= min1 && max2 <= max1)
+            {
+                return true;
+            }
+
+            if (min1 <= min2 && max1 <= max2 && max1 >= min2)
+            {
+                return true;
+            }
+
+            return min1 >= min2 && max1 >= max2 && min1 <= max2;
+        }
+    }
+}
+
---
+++ AdventOfCode2022/Tests/Day04_CampCleanupTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestFixture]
+    internal class Day04_CampCleanupTests
+    {
+        [Test]
+        public void TestFindFullyOverlappingPairs()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day04_CampCleanupTests.txt").ToArray();
+
+            // act
+            var result = Day04_CampCleanup.FindFullyOverlappingPairs(input);
+
+            // assert
+            Assert.AreEqual(498, result);
+        }
+
+        [Test]
+        public void TestFindOverlappingPairs()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day04_CampCleanupTests.txt").ToArray();
+
+            // act
+            var result = Day04_CampCleanup.FindOverlappingPairs(input);
+
+            // assert
+            Assert.AreEqual(859, result);
+        }
+    }
+}
