d411ba562e320f95590664bff3d092d7693a94c9
Scott Elder <scott.elder@relativity.com>
2022-12-06 19:06:23 -0500

    day 6 done

---
+++ AdventOfCode2022/Day06_TuningTrouble.cs
@@ -0,0 +1,23 @@
+﻿namespace AdventOfCode2022
+{
+    public static class Day06_TuningTrouble
+    {
+        public static long FindCharCountAtEndOfFirstMarker(string input, int distinctMarkerCount)
+        {
+            int lastMarkerPosition = distinctMarkerCount;
+            for (var i = 0; i < input.Length - distinctMarkerCount; i++)
+            {
+                var marker = input.ToCharArray().Take(new Range(i, i + distinctMarkerCount));
+                if (marker.Distinct().Count() == distinctMarkerCount)
+                {
+                    break;
+                }
+
+                lastMarkerPosition++;
+            }
+
+            return lastMarkerPosition;
+        }
+    }
+}
+
---
+++ AdventOfCode2022/Tests/Day06_TuningTroubleTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2022.Tests
+{
+    [TestFixture]
+    internal class Day06_TuningTroubleTests
+    {
+        [Test]
+        public void TestFindCharCountAtEndOfFirstMarkerV1()
+        {
+            // arrange 
+            var input = File.ReadAllText("Tests/Day06_TuningTroubleTests.txt");
+
+            // act
+            var result = Day06_TuningTrouble.FindCharCountAtEndOfFirstMarker(input, 4);
+
+            // assert
+            Assert.AreEqual(1920, result);
+        }
+
+        [Test]
+        public void TestFindCharCountAtEndOfFirstMarkerV2()
+        {
+            // arrange 
+            var input = File.ReadAllText("Tests/Day06_TuningTroubleTests.txt");
+
+            // act
+            var result = Day06_TuningTrouble.FindCharCountAtEndOfFirstMarker(input, 14);
+
+            // assert
+            Assert.AreEqual(2334, result);
+        }
+    }
+}
