ac0e0370d51247b40708f370f7e89826d710dc1d
Scott Elder <scottelder.tx@gmail.com>
2022-12-09 07:41:22 -0500

    Day 8, not done :(

--- AdventOfCode2022/Day08_TreetopTreeHouse.cs
+++ AdventOfCode2022/Day08_TreetopTreeHouse.cs
@@ -1,21 +1,126 @@
-﻿namespace AdventOfCode2022
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AdventOfCode2022
 {
     public static class Day08_TreetopTreeHouse
     {
         public static long CountVisibleTrees(string[] input)
         {
-            var trees = new List<List<Tree>>();
-            foreach (var row in input)
+            var treeGrid = ParseInput(input);
+
+            MarkVisibleTrees(treeGrid);
+            
+            int visibleTrees = 0;
+            foreach (var row in treeGrid)
             {
-                var newRowOfTrees = row
-                    .ToCharArray()
-                    .Select(x => new Tree((int)char.GetNumericValue(x)))
-                    .ToList();
-                trees.Add(newRowOfTrees);
+                visibleTrees += row.Count(x => x.Visible);
             }
 
-            Tree[][] treeGrid = trees.Select(x => x.ToArray()).ToArray();
+            return visibleTrees;
+        }
 
+        public static long FindMaxScenicView(string[] input)
+        {
+            var treeGrid = ParseInput(input);
+
+            for (var i = 0; i < treeGrid.Length; i++)
+            {
+                for (int j = 0; j < treeGrid[i].Length - 1; j++)
+                {
+                    // from the left, looking right
+                    var subRow = treeGrid[i].Take(new Range(j, treeGrid[i].Length)).ToArray();
+                    var scenicScore = ScoreInOneDirection(subRow);
+                    FactorInScore(treeGrid, i, j, scenicScore);
+
+                    // from the right, looking left
+                    subRow = treeGrid[i].Reverse().Take(new Range(j, treeGrid[i].Length)).ToArray();
+                    scenicScore = ScoreInOneDirection(subRow);
+                    FactorInScore(treeGrid, i, treeGrid[i].Length - 1 - j, scenicScore);
+                }
+            }
+
+            for (var j = 0; j < treeGrid[0].Length; j++)
+            {
+                var column = GetColumn(treeGrid, j);
+                for (int i = 0; i < treeGrid.Length - 1; i++)
+                {
+                    // from the top, looking down
+                    var subColumn = column.Take(new Range(i, treeGrid.Length)).ToArray();
+                    var scenicScore = ScoreInOneDirection(subColumn);
+                    FactorInScore(treeGrid, i, j, scenicScore);
+
+                    // from the bottom, looking up
+                    subColumn = column.Reverse().Take(new Range(i, treeGrid.Length)).ToArray();
+                    scenicScore = ScoreInOneDirection(subColumn);
+                    FactorInScore(treeGrid, treeGrid.Length - 1 - i, j, scenicScore);
+                }
+            }
+
+            int maxScenicView = 0;
+            foreach (var row in treeGrid)
+            {
+                int maxForRow = row.Max(x => x.ScenicScore);
+                maxScenicView = maxForRow > maxScenicView ? maxForRow : maxScenicView;
+            }
+
+            return maxScenicView;
+        }
+
+        private static void FactorInScore(Tree[][] treeGrid, int i, int j, int scenicScore)
+        {
+            if (treeGrid[i][j].ScenicScore == 0)
+            {
+                treeGrid[i][j].ScenicScore = scenicScore;
+            }
+            else
+            {
+                treeGrid[i][j].ScenicScore *= scenicScore;
+            }
+        }
+
+        private static int ScoreInOneDirection(Tree[] treeGrid)
+        {
+            var scenicScore = 0;
+            for (int i = 1; i < treeGrid.Length; i++)
+            {
+                // [home][...][a][b]
+                var a = treeGrid[i - 1].Height;
+                var b = treeGrid[i].Height;
+                var home = treeGrid[0].Height;
+
+                // if first adjacent 
+                if (i - 1 == 0)
+                {
+                    scenicScore = 1;
+                    if (home > b)
+                    {
+                        continue;
+                    }
+
+                    break;
+                }
+
+                if (a <= b)
+                {
+                    scenicScore++;
+                }
+                else
+                {
+                    break;
+                }
+
+                if (home <= b)
+                {
+                    break;
+                }
+            }
+
+            return scenicScore;
+        }
+
+        private static void MarkVisibleTrees(Tree[][] treeGrid)
+        {
             // mark top and bottom rows as visible
             foreach (var tree in treeGrid[0])
             {
@@ -28,7 +133,7 @@
             }
 
             // mark first and column as visible
-            for (var i = 0; i < treeGrid.LongLength; i++)
+            for (var i = 0; i < treeGrid[0].Length; i++)
             {
                 treeGrid[i][0].Visible = true;
                 treeGrid[i][^1].Visible = true;
@@ -36,7 +141,7 @@
 
             // "viewed" from left 
             var previousHeight = 0;
-            foreach (var row in trees)
+            foreach (var row in treeGrid)
             {
                 previousHeight = 0;
                 foreach (var tree in row)
@@ -50,10 +155,10 @@
             }
 
             // "viewed" from right 
-            foreach (var row in trees)
+            foreach (var row in treeGrid)
             {
                 previousHeight = 0;
-                for (var j = row.Count - 1; j >= 0; j--)
+                for (var j = row.Length - 1; j >= 0; j--)
                 {
                     var tree = row[j];
                     if (tree.Height > previousHeight)
@@ -78,8 +183,7 @@
                     }
                 }
             }
-
-
+            
             // "viewed" from bottom
             for (var j = 0; j < treeGrid[0].Length; j++)
             {
@@ -94,21 +198,37 @@
                     }
                 }
             }
+        }
 
+        private static Tree[] GetColumn(Tree[][] matrix, int j)
+        {
+            return Enumerable.Range(0, matrix.GetLength(0))
+                .Select(x => matrix[x][j])
+                .ToArray();
+        }
 
-            int visibleTrees = 0;
-            foreach (var row in trees)
+        private static Tree[][] ParseInput(string[] input)
+        {
+            var trees = new List<List<Tree>>();
+            foreach (var row in input)
             {
-                visibleTrees += row.Count(x => x.Visible);
+                var newRowOfTrees = row
+                    .ToCharArray()
+                    .Select(x => new Tree((int)char.GetNumericValue(x)))
+                    .ToList();
+                trees.Add(newRowOfTrees);
             }
 
-            return visibleTrees;
+            Tree[][] treeGrid = trees.Select(x => x.ToArray()).ToArray();
+            return treeGrid;
         }
 
         public class Tree
         {
             public bool Visible { get; set; }
             public int Height { get; }
+
+            public int ScenicScore { get; set; }
 
             public Tree(int height)
             {
--- AdventOfCode2022/Tests/Day08_TreetopTreeHouseTests.cs
+++ AdventOfCode2022/Tests/Day08_TreetopTreeHouseTests.cs
@@ -6,13 +6,26 @@ namespace AdventOfCode2022.Tests
     internal class Day08_TreetopTreeHouseTests
     {
         [Test]
-        public void TestSomeAction()
+        public void TestCountVisibleTrees()
         {
             // arrange 
             var input = File.ReadLines("Tests/Day08_TreetopTreeHouseTests.txt").ToArray();
 
             // act
             var result = Day08_TreetopTreeHouse.CountVisibleTrees(input);
+
+            // assert
+            Assert.AreEqual(1688, result);
+        }
+
+        [Test]
+        public void TestFindMaxScenicView()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day08_TreetopTreeHouseTests.txt").ToArray();
+
+            // act
+            var result = Day08_TreetopTreeHouse.FindMaxScenicView(input);
 
             // assert
             Assert.AreEqual(1688, result);
