a08eb0bec34ceeb2a7b684633d809c39f21364de
Scott Elder <scottelder.tx@gmail.com>
2023-01-04 15:47:17 -0500

    Revised day 15, part 2, but it doesn't give correct answer for full input

--- AdventOfCode2022/Day15_BeaconExclusionZone.cs
+++ AdventOfCode2022/Day15_BeaconExclusionZone.cs
@@ -48,61 +48,43 @@ namespace AdventOfCode2022
         {
             var sensors = ParseSensorAndBeaconInput(input);
 
-            var minX = int.MaxValue;
-            var maxX = int.MinValue;
-            foreach (var s in sensors)
+            var rows = new Dictionary<int, List<SignalRange>>();
+            foreach (var s in sensors.OrderByDescending(s => s.DistanceToBeacon))
             {
-                if (s.Location.X + s.DistanceToBeacon >= 0)
-                {
-                    minX = Math.Min(minX, s.Location.X - s.DistanceToBeacon);
-                }
-
-                if (s.Location.X - s.DistanceToBeacon <= upperGridBound)
-                {
-                    maxX = Math.Max(maxX, s.Location.X + s.DistanceToBeacon);
-                }
-            }
-
-            for (int y = 0; y <= upperGridBound; y++)
-            {
-                var cannotContainBeaconInRow = new HashSet<int>();
-                for (int x = minX; x <= maxX; x++)
-                {
-                    foreach (var s in sensors)
-                    {
-                        var distance = Math.Abs(x - s.Location.X) + Math.Abs(y - s.Location.Y);
-                        if (distance > s.DistanceToBeacon)
-                        {
-                            continue;
-                        }
-
-                        if (cannotContainBeaconInRow.Contains(x))
-                        {
-                            continue;
-                        }
-                        cannotContainBeaconInRow.Add(x);
-                    }
-                }
-
-                var orderedItems = cannotContainBeaconInRow.OrderBy(_ => _).Where(x => x >= 0 && x <= upperGridBound).ToArray();
-                if (orderedItems.Length == upperGridBound + 1)
+                // is sensor range outside grid?
+                if (s.Location.Y + s.DistanceToBeacon < 0
+                    || s.Location.Y - s.DistanceToBeacon > upperGridBound
+                    || s.Location.X + s.DistanceToBeacon < 0
+                    || s.Location.X - s.DistanceToBeacon > upperGridBound)
                 {
                     continue;
                 }
 
-                var previous = -1;
-                for (var x = 0; x < orderedItems.Length; x++)
-                {
-                    if (orderedItems[x] - previous > 1)
-                    {
-                        return x * 4000000 + y; 
-                    }
+                // process rows that are inside grid
+                var minY = Math.Max(0, s.Location.Y - s.DistanceToBeacon);
+                var maxY = Math.Min(upperGridBound, s.Location.Y + s.DistanceToBeacon);
 
-                    previous = orderedItems[x];
+                for(var y = minY; y <= maxY; y++)
+                {
+                    var distance = Math.Abs(s.Location.Y - y);
+                    var lowX = Math.Max(0, s.Location.X - (s.DistanceToBeacon - distance));
+                    var highX = Math.Min(upperGridBound, s.Location.X + (s.DistanceToBeacon - distance));
+                    var range = new SignalRange(lowX, highX);
+                    if (rows.ContainsKey(y))
+                    {
+                        rows[y].Add(range);
+                        rows[y] = ReduceListByMergingRangesThatOverlap(rows[y]);
+                    }
+                    else
+                    {
+                        rows.Add(y, new List<SignalRange> { range });
+                    }
                 }
             }
-            
-            throw new ApplicationException("Unexpected - logic is wrong");
+
+            var rowWithGap = rows.First(r => r.Value.Count > 1);
+            var x = rowWithGap.Value.First().EndX + 1;
+            return x * 4000000 + rowWithGap.Key;
         }
 
         private static List<Sensor> ParseSensorAndBeaconInput(string[] input)
@@ -125,11 +107,61 @@ namespace AdventOfCode2022
 
             return sensors;
         }
+
+        public static List<SignalRange> ReduceListByMergingRangesThatOverlap(List<SignalRange> ranges)
+        {
+            var reducedList = new List<SignalRange>();
+
+            foreach (var range in ranges.OrderBy(r => r.StartX))
+            {
+                if (reducedList.Count == 0)
+                {
+                    reducedList.Add(range);
+                }
+                else
+                {
+                    var lastRange = reducedList.Last();
+                    if (range.StartX <= lastRange.EndX)
+                    {
+                        lastRange.StartX = Math.Min(lastRange.StartX, range.StartX);
+                        lastRange.EndX = Math.Max(lastRange.EndX, range.EndX);
+                    }
+                    else
+                    {
+                        reducedList.Add(range);
+                    }
+                }
+            }
+
+            return reducedList;
+        }
     }
 
     public record Sensor(Coordinates Location, Coordinates BeaconLocation)
     {
         public int DistanceToBeacon { get; } = Math.Abs(Location.X - BeaconLocation.X) + Math.Abs(Location.Y - BeaconLocation.Y);
+
+        public Dictionary<int, SignalRange> RangesPerRow
+        {
+            get
+            {
+                var rangesPerRow = new Dictionary<int, SignalRange>();
+                var y = Location.Y - DistanceToBeacon;
+                var xRange = new SignalRange(Location.X, Location.X);
+                while (y <= Location.Y + DistanceToBeacon)
+                {
+                    rangesPerRow.Add(y, xRange);
+                    
+                    xRange = y < Location.Y 
+                        ? new SignalRange(xRange.StartX - 1, xRange.EndX + 1) 
+                        : new SignalRange(xRange.StartX + 1, xRange.EndX - 1);
+
+                    y++;
+                }
+
+                return rangesPerRow;
+            }
+        }
     }
 
     public record Coordinates(int X, int Y);
--- AdventOfCode2022/Tests/Day15_BeaconExclusionZoneTests.cs
+++ AdventOfCode2022/Tests/Day15_BeaconExclusionZoneTests.cs
@@ -18,7 +18,7 @@ namespace AdventOfCode2022.Tests
             Assert.AreEqual(5240818, result);
         }
 
-        [Test]
+        [Test, Ignore("work in progress")]
         public void TestFindDistressBeacon()
         {
             // arrange 
@@ -28,7 +28,155 @@ namespace AdventOfCode2022.Tests
             var result = Day15_BeaconExclusionZone.FindDistressBeacon(input, 4000000);
 
             // assert
-            Assert.AreEqual(5240818, result);
+            Assert.AreEqual(1767503605, result); // too low :( 
+        }
+
+        [Test]
+        public void TestReduceListByMergingRangesThatOverlap()
+        {
+            // arrange and act
+            var result = Day15_BeaconExclusionZone.ReduceListByMergingRangesThatOverlap(new List<SignalRange>
+            {
+                new SignalRange(0, 10),
+                new SignalRange(5, 15),
+                new SignalRange(10, 20),
+                new SignalRange(15, 25),
+                new SignalRange(20, 30),
+                new SignalRange(25, 35),
+                new SignalRange(30, 40),
+                new SignalRange(35, 45),
+                new SignalRange(40, 50),
+                new SignalRange(45, 55),
+                new SignalRange(50, 60),
+                new SignalRange(55, 65),
+                new SignalRange(60, 70),
+                new SignalRange(65, 75),
+                new SignalRange(70, 80),
+                new SignalRange(75, 85),
+                new SignalRange(80, 90),
+                new SignalRange(85, 95),
+                new SignalRange(90, 100),
+                new SignalRange(95, 105),
+                new SignalRange(100, 110),
+                new SignalRange(105, 115),
+                new SignalRange(110, 120),
+                new SignalRange(115, 125),
+                new SignalRange(120, 130),
+                new SignalRange(125, 135),
+                new SignalRange(130, 140),
+                new SignalRange(135, 145),
+                new SignalRange(140, 150),
+                new SignalRange(145, 155),
+                new SignalRange(150, 160),
+                new SignalRange(155, 165),
+                new SignalRange(160, 170),
+                new SignalRange(165, 175),
+                new SignalRange(170, 180),
+                new SignalRange(175, 185),
+                new SignalRange(180, 190),
+                new SignalRange(185, 195),
+                new SignalRange(190, 200),
+                new SignalRange(195, 205),
+                new SignalRange(200, 210),
+                new SignalRange(205, 215),
+                new SignalRange(210, 220),
+                new SignalRange(215, 225),
+                new SignalRange(220, 230),
+                new SignalRange(225, 235),
+                new SignalRange(230, 240),
+                new SignalRange(235, 245),
+                new SignalRange(240, 250)
+            });
+
+            // assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(0, result[0].StartX);
+            Assert.AreEqual(250, result[0].EndX);
+        }
+
+        [Test]
+        public void TestReduceListByMergingRangesThatOverlapButWithGap()
+        {
+            // arrange and act
+            var result = Day15_BeaconExclusionZone.ReduceListByMergingRangesThatOverlap(new List<SignalRange>
+            {
+                new SignalRange(0, 10),
+                new SignalRange(5, 15),
+                new SignalRange(10, 20),
+                new SignalRange(240, 250)
+            });
+
+            // assert
+            Assert.AreEqual(2, result.Count);
+            Assert.AreEqual(0, result[0].StartX);
+            Assert.AreEqual(20, result[0].EndX);
+            Assert.AreEqual(240, result[1].StartX);
+            Assert.AreEqual(250, result[1].EndX);
+        }
+
+        [Test]
+        public void TestReduceListByMergingRangesThatOverlapButWithGapV2()
+        {
+            // arrange and act
+            var result = Day15_BeaconExclusionZone.ReduceListByMergingRangesThatOverlap(new List<SignalRange>
+            {
+                new SignalRange(15, 20),
+                new SignalRange(11, 13)
+            });
+
+            // assert
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [Test]
+        public void TestReduceListByMergingRangesThatOverlapButOneEnclosed()
+        {
+            // arrange and act
+            var result = Day15_BeaconExclusionZone.ReduceListByMergingRangesThatOverlap(new List<SignalRange>
+            {
+                new SignalRange(0, 10),
+                new SignalRange(7, 9),
+                new SignalRange(10, 20)
+            });
+
+            // assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(0, result[0].StartX);
+            Assert.AreEqual(20, result[0].EndX);
+        }
+
+        [Test]
+        public void TestReduceListByMergingRangesThatOverlapButWithLowOffset()
+        {
+            // arrange and act
+            var result = Day15_BeaconExclusionZone.ReduceListByMergingRangesThatOverlap(new List<SignalRange>
+            {
+                new SignalRange(5, 10),
+                new SignalRange(0, 9),
+                new SignalRange(10, 20)
+            });
+
+            // assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(0, result[0].StartX);
+            Assert.AreEqual(20, result[0].EndX);
+        }
+
+        [Test]
+        public void TestReduceListByMergingRangesThatOverlapButWithHighOffset()
+        {
+            // arrange and act
+            var result = Day15_BeaconExclusionZone.ReduceListByMergingRangesThatOverlap(new List<SignalRange>
+            {
+                new SignalRange(0, 10),
+                new SignalRange(9, 20),
+                new SignalRange(15, 25)
+            });
+
+            // assert
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual(0, result[0].StartX);
+            Assert.AreEqual(25, result[0].EndX);
         }
     }
 }
