6afb2c5170f76c0f77fc3a55539b6ac5a50fd241
Scott Elder <selder@humrro.org>
2023-12-02 08:14:46 -0500

    Done with part I, day 2

---
+++ AdventOfCode2023/Day02_CubeConundrum.cs
@@ -0,0 +1,64 @@
+﻿namespace AdventOfCode2023
+{
+    public static class Day02_CubeConundrum
+    {
+        public static long SumIdsOfPossibleGames(string[] input)
+        {
+            // possible: 12 red cubes, 13 green cubes, and 14 blue cubes
+            // not possible: if any hand contains value greater (e.g., 20 red cubes)
+            // not possible: if any hand contains more cubes than 39
+
+            var sum = 0;
+            foreach (var game in input)
+            {
+                var delimiters = new string[] { "Game ", ": ", "; " };
+                var parts = game.Split(delimiters, StringSplitOptions.None);
+                int id = int.Parse(parts[1]);
+                bool validGame = true;
+                for (int i = 2; i < parts.Length; i++)
+                {
+                    var blue = 0;
+                    var red = 0;
+                    var green = 0;
+                    var diceTotalsPerRound = parts[i].Split(", ");
+                    foreach( var dice in diceTotalsPerRound)
+                    {
+                        var numberAndColor = dice.Split(' ');
+                        if (numberAndColor[1] == "blue")
+                        {
+                            blue = int.Parse(numberAndColor[0]);
+                        }
+
+                        if (numberAndColor[1] == "red")
+                        {
+                            red = int.Parse(numberAndColor[0]);
+                        }
+
+                        if (numberAndColor[1] == "green")
+                        {
+                            green = int.Parse(numberAndColor[0]);
+                        }
+                    }
+                    
+
+                    if (blue + red + green > 39 ||
+                        blue > 14 ||
+                        green > 13 ||
+                        red > 12)
+                    {
+                        validGame = false;
+                        break;
+                    }
+                }
+
+                if (validGame)
+                {
+                    sum += id;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
+
---
+++ AdventOfCode2023/Tests/Day02_CubeConundrumTests.cs
@@ -0,0 +1,22 @@
+﻿
+using NUnit.Framework;
+
+namespace AdventOfCode2023.Tests
+{
+    [TestFixture]
+    internal class Day02_CubeConundrumTests
+    {
+        [Test]
+        public void TestSumIdsOfPossibleGames()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day02_CubeConundrumTests.txt").ToArray();
+
+            // act
+            var result = Day02_CubeConundrum.SumIdsOfPossibleGames(input);
+
+            // assert
+            Assert.AreEqual(1867, result);
+        }
+    }
+}
