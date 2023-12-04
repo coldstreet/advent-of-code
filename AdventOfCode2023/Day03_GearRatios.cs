1b5b65fde6586135bebb28db9f634d34c7912e3c
Scott Elder <selder@humrro.org>
2023-12-03 19:27:06 -0500

    stuck on days (answer too high)

--- AdventOfCode2023/Day03_GearRatios.cs
+++ AdventOfCode2023/Day03_GearRatios.cs
@@ -4,7 +4,136 @@
     {
         public static long SumPartNumbers(string[] input)
         {
-            return 0;
+            // build grid
+            var grid = new char[input.Length, input[0].Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    grid[i, j] = input[i][j];
+                }
+            }
+
+            var parts = new Dictionary<int, int>();
+            List<char> numbers = new List<char>();
+            bool isPart = false;
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < input[i].Length; j++)
+                {
+                    var c = grid[i, j];
+                    if (char.IsNumber(c))
+                    {
+                        numbers.Add(c);
+                        if (DoesNumberInGridHavePartIdNearBy(i, j, grid))
+                        {
+                            isPart = true;
+                        }
+                    }
+                    else
+                    {
+                        if (numbers.Count > 0)
+                        {
+                            int number = BuildNumber(numbers);
+                            if (!parts.ContainsKey(number))
+                            {
+                                parts.Add(number, 0);
+                            }
+
+                            if (isPart)
+                            {
+                                parts[number]++;
+                            }
+                        }
+
+                        numbers.Clear();
+                        isPart = false;
+                    }
+                }
+            }
+
+            int sum = 0;
+            foreach(int key in parts.Keys)
+            {
+                sum += key * parts[key];
+            }
+            
+            return sum;
+        }
+
+        private static bool DoesNumberInGridHavePartIdNearBy(int i, int j, char[,] grid)
+        {
+            int maxI = grid.GetLength(0);
+            int maxJ = grid.GetLength(1);
+            int up = i - 1 >= 0 ? i - 1 : i;
+            int down = i + 1 < maxI ? i + 1 : i;
+            int left = j - 1 >= 0 ? j - 1 : j;
+            int right = j + 1 < maxJ ? j + 1 : j;
+
+            if (HasPartSymbolAdjecent(grid[up, left]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[up, j]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[up, right]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[i, left]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[i, right]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[down, left]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[down, j]))
+            {
+                return true;
+            }
+
+            if (HasPartSymbolAdjecent(grid[down, right]))
+            {
+                return true;
+            }
+
+
+            return false;
+        }
+
+        private static int BuildNumber(IEnumerable<char> numbers)
+        {
+            char[] chars = numbers.ToArray();
+            string s = new string(chars);
+            return int.Parse(s);
+        }
+
+        private static bool HasPartSymbolAdjecent(char c)
+        {
+            if (char.IsNumber(c))
+            {
+                return false;
+            }
+
+            if (c == '.')
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
