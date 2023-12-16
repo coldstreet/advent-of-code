99bf143573647453e4736c14036b22071feae7ca
Scott Elder <scottelder.tx@gmail.com>
2023-12-16 12:53:54 -0500

    work in progress

--- AdventOfCode2023/Day04_Scratchcards.cs
+++ AdventOfCode2023/Day04_Scratchcards.cs
@@ -22,15 +22,69 @@
             return sum;
         }
 
+        public static long CountCardsAfterWinnings(string[] input)
+        {
+            var cards = ParseInputForMultipleCards(input);
+            foreach(var key in cards.Keys)
+            {
+                // process the first card in a set of card #
+                var card = cards[key].First();
+                int count = 0;
+                foreach (var number in card.Numbers)
+                {
+                    if (card.Keys.Contains(number))
+                    {
+                        count++;
+                    }
+                }
+
+                // Now add additional cards to preceeding sets (but don't go past last set of card #)
+                foreach (var _ in cards[key])
+                {
+                    for (int k = key + 1; k <= Math.Min(card.Keys.Count, key + count); k++)
+                    {
+                        cards[k].Add(cards[k].First());
+                    }
+                }
+            }
+
+            int sum = 0;
+            foreach (var cardList in cards.Values)
+            {
+                sum += cardList.Count;
+            }
+
+            return sum;
+        }
+
         private static IDictionary<int, Card> ParseInput(string[] input)
         {
             var cards = new Dictionary<int, Card>();
             foreach (var item in input)
             {
-                var parts = item.Split(new string[] { "Card ", ": ", " | " }, StringSplitOptions.RemoveEmptyEntries);
-                var keys = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-                var numbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
-                cards.Add(int.Parse(parts[0]), new Card(keys, numbers));
+                var card = ParseInputLine(item);
+                cards.Add(card.Id, card);
+            }
+
+            return cards;
+        }
+
+        private static Card ParseInputLine(string item)
+        {
+            var parts = item.Split(new string[] { "Card ", ": ", " | " }, StringSplitOptions.RemoveEmptyEntries);
+            var keys = parts[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            var numbers = parts[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            return new Card(int.Parse(parts[0]), keys, numbers);
+        }
+
+        private static IDictionary<int, IList<Card>> ParseInputForMultipleCards(string[] input)
+        {
+            var cards = new Dictionary<int, IList<Card>>();
+            foreach (var item in input)
+            {
+                var card = ParseInputLine(item);
+                cards.Add(card.Id, new List<Card> { new Card(card.Id, card.Keys, card.Numbers) });
             }
 
             return cards;
@@ -39,14 +93,17 @@
 
     public class Card
     {
+        public int Id { get; set; }
+
         public IList<int> Keys { get; set; }
 
         public IList<int> Numbers { get; set; }
 
         public long Score { get; set; }
 
-        public Card(IList<int> keys, IList<int> numbers)
+        public Card(int id, IList<int> keys, IList<int> numbers)
         {
+            Id = id;
             Keys = keys;
             Numbers = numbers;
         }
--- AdventOfCode2023/Tests/Day04_ScratchcardsTests.cs
+++ AdventOfCode2023/Tests/Day04_ScratchcardsTests.cs
@@ -18,5 +18,18 @@ namespace AdventOfCode2023.Tests
             // assert
             Assert.AreEqual(21138, result);
         }
+
+        [Test]
+        public void TestCountCardsAfterWinnings()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day04_ScratchcardsTests.txt").ToArray();
+
+            // act
+            var result = Day04_Scratchcards.CountCardsAfterWinnings(input);
+
+            // assert
+            Assert.AreEqual(21138, result);
+        }
     }
 }
