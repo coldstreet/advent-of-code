672d6cd1f5b04b3cf85567029b1f74a5385a7220
Scott Elder <scottelder.tx@gmail.com>
2023-12-17 11:58:23 -0500

    Added files for day 5

---
+++ AdventOfCode2023/Day05_IfYouGiveSeedFertilizer.cs
@@ -0,0 +1,11 @@
+﻿namespace AdventOfCode2023
+{
+    public static class Day05_IfYouGiveSeedFertilizer
+    {
+        public static long DetermineLowestLocationNumber(string[] input)
+        {
+            return 0;
+        }
+    }
+}
+
---
+++ AdventOfCode2023/Tests/Day05_IfYouGiveSeedFertilizerTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace AdventOfCode2023.Tests
+{
+    [TestFixture]
+    internal class Day05_IfYouGiveSeedFertilizerTests
+    {
+        [Test]
+        public void TestSomeAction()
+        {
+            // arrange 
+            var input = File.ReadLines("Tests/Day05_IfYouGiveSeedFertilizerTests.txt").ToArray();
+
+            // act
+            var result = Day05_IfYouGiveSeedFertilizer.DetermineLowestLocationNumber(input);
+
+            // assert
+            Assert.AreEqual(0, result);
+        }
+    }
+}
